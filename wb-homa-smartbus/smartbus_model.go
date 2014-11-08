e566795c958d1ecb00afae6836b621706d528fe9
Ivan Shvedunov <ivan4th@gmail.com>
2014-11-09 02:24:10 +0300

    smartbus: implemented SmartbusModel.

---
+++ wb-homa-smartbus/smartbus_model.go
@@ -0,0 +1,123 @@
+package smartbus
+
+import (
+	"fmt"
+	"log"
+	"io"
+	"strings"
+	"strconv"
+)
+
+type SmartbusDeviceItem struct {
+	Name, Title string
+}
+
+var smartbusDeviceTypes map[uint16]SmartbusDeviceItem = map[uint16]SmartbusDeviceItem{
+	0x139c: {"zonebeast", "Zone Beast"},
+}
+
+func smartbusNameFromHeader(header *MessageHeader) (string, string, bool) {
+	item, found := smartbusDeviceTypes[header.OrigDeviceType]
+	if !found {
+		log.Printf("unhandled device type: %04x", header.OrigDeviceType)
+		return "", "", false
+	}
+	name := fmt.Sprintf("%s%02x%02x", item.Name, header.OrigSubnetID, header.OrigDeviceID)
+	title := fmt.Sprintf("%s %02x:%02x", item.Title, header.OrigSubnetID, header.OrigDeviceID)
+	return name, title, true
+}
+
+type SmartbusModel struct {
+	ModelBase
+	deviceMap map[string]*SmartbusModelDevice
+	ep *SmartbusEndpoint
+}
+
+func NewSmartbusModel(stream io.ReadWriteCloser, subnetID uint8,
+	deviceID uint8, deviceType uint16) (model *SmartbusModel) {
+	model = &SmartbusModel{ deviceMap: make(map[string]*SmartbusModelDevice) }
+	conn := NewSmartbusConnection(stream)
+	model.ep = conn.MakeSmartbusEndpoint(subnetID, deviceID, deviceType)
+	model.ep.SetHandler(model)
+	return
+}
+
+func (model *SmartbusModel) QueryDevices() {
+	// NOOP
+}
+
+func (model *SmartbusModel) ensureDevice(header *MessageHeader) *SmartbusModelDevice {
+	name, title, found := smartbusNameFromHeader(header)
+	if !found {
+		return nil
+	}
+	var dev *SmartbusModelDevice
+	dev, found = model.deviceMap[name]
+	if found {
+		return dev
+	}
+	
+	dev = &SmartbusModelDevice{}
+	dev.DevName = name
+	dev.DevTitle = title
+	dev.smartDev = model.ep.GetSmartbusDevice(header.OrigSubnetID, header.OrigDeviceID)
+	dev.channelStatus = make([]bool, 0, 100)
+	model.deviceMap[name] = dev
+	model.Observer.OnNewDevice(dev)
+
+	return dev
+}
+
+func (model *SmartbusModel) OnZoneBeastBroadcast(msg *ZoneBeastBroadcast, header *MessageHeader) {
+	dev := model.ensureDevice(header)
+	if dev != nil {
+		dev.updateChannelStatus(msg.ChannelStatus)
+	}
+}
+
+type SmartbusModelDevice struct {
+	DeviceBase
+	smartDev *SmartbusDevice
+	channelStatus []bool
+}
+
+func (dev *SmartbusModelDevice) updateChannelStatus(channelStatus []bool) {
+	updateCount := len(dev.channelStatus)
+	if updateCount > len(channelStatus) {
+		updateCount = len(channelStatus)
+	}
+
+	for i := 0; i < updateCount; i++ {
+		if dev.channelStatus[i] == channelStatus[i] {
+			continue
+		}
+		v := "0"
+		dev.channelStatus[i] = channelStatus[i]
+		if dev.channelStatus[i] {
+			v = "1"
+		}
+		dev.Observer.OnValue(dev, fmt.Sprintf("Channel %d", i + 1), v)
+	}
+
+	for i := updateCount; i < len(channelStatus); i++ {
+		dev.channelStatus = append(dev.channelStatus, channelStatus[i])
+		v := "0"
+		if dev.channelStatus[i] {
+			v = "1"
+		}
+		controlName := fmt.Sprintf("Channel %d", i + 1)
+		dev.Observer.OnNewControl(dev, controlName, "switch", v)
+	}
+}
+
+func (dev *SmartbusModelDevice) SendValue(name, value string) {
+	channelNo, err := strconv.Atoi(strings.TrimPrefix(name, "Channel "))
+	if err != nil {
+		log.Printf("bad channel name: %s", name)
+	}
+	level := uint8(LIGHT_LEVEL_OFF)
+	if value == "1" {
+		level = LIGHT_LEVEL_ON
+	}
+	dev.smartDev.SingleChannelControl(uint8(channelNo), level, 0)
+}
---
+++ wb-homa-smartbus/smartbus_model_test.go
@@ -0,0 +1,84 @@
+package smartbus
+
+import (
+	"net"
+	"testing"
+)
+
+func TestSmartbusDriver(t *testing.T) {
+	p, r := net.Pipe()
+
+	broker := NewFakeMQTTBroker(t)
+	model := NewSmartbusModel(p, SAMPLE_SUBNET, SAMPLE_ORIG_DEVICE_ID, SAMPLE_ORIG_DEVICE_TYPE)
+	client := broker.MakeClient("tst", func (msg MQTTMessage) {
+		t.Logf("tst: message %v", msg)
+	})
+	client.Start()
+	driver := NewDriver(model, func (handler MQTTMessageHandler) MQTTClient {
+		return broker.MakeClient("driver", handler)
+	})
+
+	handler := NewFakeHandler(t)
+	conn := NewSmartbusConnection(r)
+	ep := conn.MakeSmartbusEndpoint(
+		SAMPLE_SUBNET, SAMPLE_TARGET_DEVICE_ID, SAMPLE_TARGET_DEVICE_TYPE)
+	ep.SetHandler(handler)
+	dev := ep.GetBroadcastDevice()
+
+	driver.Start()
+
+	dev.ZoneBeastBroadcast([]byte{ 0 }, parseChannelStatus("---x"))
+	broker.Verify(
+		"driver -> /devices/zonebeast011c/meta/name: [Zone Beast 01:1c] (QoS 1, retained)",
+
+		"driver -> /devices/zonebeast011c/controls/Channel 1/meta/type: [switch] (QoS 1, retained)",
+		"driver -> /devices/zonebeast011c/controls/Channel 1/meta/order: [1] (QoS 1, retained)",
+		"driver -> /devices/zonebeast011c/controls/Channel 1: [0] (QoS 1, retained)",
+		"Subscribe -- driver: /devices/zonebeast011c/controls/Channel 1/on",
+
+		"driver -> /devices/zonebeast011c/controls/Channel 2/meta/type: [switch] (QoS 1, retained)",
+		"driver -> /devices/zonebeast011c/controls/Channel 2/meta/order: [2] (QoS 1, retained)",
+		"driver -> /devices/zonebeast011c/controls/Channel 2: [0] (QoS 1, retained)",
+		"Subscribe -- driver: /devices/zonebeast011c/controls/Channel 2/on",
+
+		"driver -> /devices/zonebeast011c/controls/Channel 3/meta/type: [switch] (QoS 1, retained)",
+		"driver -> /devices/zonebeast011c/controls/Channel 3/meta/order: [3] (QoS 1, retained)",
+		"driver -> /devices/zonebeast011c/controls/Channel 3: [0] (QoS 1, retained)",
+		"Subscribe -- driver: /devices/zonebeast011c/controls/Channel 3/on",
+
+		"driver -> /devices/zonebeast011c/controls/Channel 4/meta/type: [switch] (QoS 1, retained)",
+		"driver -> /devices/zonebeast011c/controls/Channel 4/meta/order: [4] (QoS 1, retained)",
+		"driver -> /devices/zonebeast011c/controls/Channel 4: [1] (QoS 1, retained)",
+		"Subscribe -- driver: /devices/zonebeast011c/controls/Channel 4/on",
+	)
+
+	dev.ZoneBeastBroadcast([]byte{ 0 }, parseChannelStatus("x---"))
+	broker.Verify(
+		"driver -> /devices/zonebeast011c/controls/Channel 1: [1] (QoS 1, retained)",
+		"driver -> /devices/zonebeast011c/controls/Channel 4: [0] (QoS 1, retained)",
+	)
+
+	client.Publish(MQTTMessage{"/devices/zonebeast011c/controls/Channel 2/on", "1", 1, false})
+	broker.Verify(
+		"tst -> /devices/zonebeast011c/controls/Channel 2/on: [1] (QoS 1)",
+		"driver -> /devices/zonebeast011c/controls/Channel 2: [1] (QoS 1, retained)",
+	)
+	handler.Verify("01/14 (type 0095) -> 01/1c: <SingleChannelControlCommand 2/100/0>")
+
+	client.Publish(MQTTMessage{"/devices/zonebeast011c/controls/Channel 1/on", "0", 1, false})
+	broker.Verify(
+		"tst -> /devices/zonebeast011c/controls/Channel 1/on: [0] (QoS 1)",
+		"driver -> /devices/zonebeast011c/controls/Channel 1: [0] (QoS 1, retained)",
+	)
+	handler.Verify("01/14 (type 0095) -> 01/1c: <SingleChannelControlCommand 1/0/0>")
+
+	// TBD: off (ch 1)
+
+	driver.Stop()
+	conn.Close()
+	broker.Verify(
+		"stop: driver",
+	)
+}
+
+// TBD: handle SingleChannelControlResponse status values
