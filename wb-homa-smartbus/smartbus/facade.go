c8599879fee75c2e98d7abc41c57fba9b33cfeed
Ivan Shvedunov <ivan4th@gmail.com>
2014-11-19 06:56:50 +0300

    smartbus: direct serial port access support.

--- wb-homa-smartbus/smartbus/facade.go
+++ wb-homa-smartbus/smartbus/facade.go
@@ -1,7 +1,10 @@
 package smartbus
 
 import (
+	"io"
 	"net"
+	"strings"
+        serial "github.com/ivan4th/goserial"
 )
 
 // FIXME
@@ -12,8 +15,21 @@ const (
 	DRIVER_CLIENT_ID = "smartbus"
 )
 
+func connect(serialAddress string) (io.ReadWriteCloser, error) {
+	if strings.HasPrefix(serialAddress, "/") {
+		return serial.OpenPort(&serial.Config{
+			Name: serialAddress,
+			Baud: 9600,
+			Parity: serial.ParityEven,
+			Size: serial.Byte8,
+		})
+	} else {
+		return net.Dial("tcp", serialAddress)
+	}
+}
+
 func NewSmartbusTCPDriver(serialAddress, brokerAddress string) (*Driver, error) {
-	conn, err := net.Dial("tcp", serialAddress)
+	conn, err := connect(serialAddress)
 	if err != nil {
 		return nil, err
 	}
--- wb-homa-smartbus/smartbus_driver.go
+++ wb-homa-smartbus/smartbus_driver.go
@@ -7,7 +7,7 @@ import (
 )
 
 func main () {
-	serial := flag.String("serial", "localhost:10011", "the address of serial port redirector")
+	serial := flag.String("serial", "/dev/ttyNSC1", "serial port address (/dev/... or host:port)")
 	broker := flag.String("broker", "tcp://localhost:1883", "MQTT broker url")
 	flag.Parse()
 	if driver, err := smartbus.NewSmartbusTCPDriver(*serial, *broker); err != nil {
