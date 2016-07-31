cbf7f5aad06c8ea351c30b2294e1e17aab581158
Sergey Skorokhodov <ucayalyfish@gmail.com>
2016-07-31 21:21:14 +0300

    Учтено различное расположение заголовочных файлов в linux и OS X.

--- wb-mqtt-serial/serial_config.h
+++ wb-mqtt-serial/serial_config.h
@@ -8,7 +8,12 @@
 
 #include "register.h"
 #include "portsettings.h"
-#include <json/json.h>
+
+#if defined(__APPLE__) || defined(__APPLE_CC__)
+#   include <json/json.h>
+#else
+#   include <jsoncpp/json/json.h>
+#endif
 
 struct TTemplate {
     TTemplate(const Json::Value& device_data);
