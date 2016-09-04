6a18cb3291307e70191549c3d51c8359ec3218c3
Sergey Skorokhodov <ucayalyfish@yandex.ru>
2016-09-05 02:14:18 +0300

    Подготовка к правильному использованию BCD32. Тесты пока сломаны.

--- wb-mqtt-serial/mercury200_02_device.h
+++ wb-mqtt-serial/mercury200_02_device.h
@@ -22,13 +22,9 @@ public:
     };
 
     TMercury20002Device(PDeviceConfig config, PAbstractSerialPort port);
-
     virtual ~TMercury20002Device();
-
     virtual uint64_t ReadRegister(PRegister reg);
-
     virtual void WriteRegister(PRegister reg, uint64_t value);
-
     virtual void EndPollCycle();
 
 private:
@@ -36,31 +32,23 @@ private:
     {
         uint32_t values[4];
     };
-
-    const TMercury20002Device::TEnergyValues &ReadEnergyValues(uint32_t slave);
+    const TMercury20002Device::TEnergyValues& ReadEnergyValues(uint32_t slave);
 
     struct TParamValues
     {
         uint32_t values[3];
     };
-
-    const TMercury20002Device::TParamValues &ReadParamValues(uint32_t slave);
-
+    const TMercury20002Device::TParamValues& ReadParamValues(uint32_t slave);
     // buf expected to be 7 bytes long
-    void FillCommand(uint8_t *buf, uint32_t id, uint8_t cmd) const;
-
-    int RequestResponse(uint32_t slave, uint8_t cmd, uint8_t *response) const;
-
+    void FillCommand(uint8_t* buf, uint32_t id, uint8_t cmd) const;
+    int RequestResponse(uint32_t slave, uint8_t cmd, uint8_t* response) const;
     bool BadHeader(uint32_t slave_expected, uint8_t cmd_expected, uint8_t* response) const;
-
-    uint32_t DecodeBCD(uint8_t *pb, int how_many) const;
-
-    bool CRCInvalid(uint8_t *buf, int sz) const;
+    // alignment independent cast of up to four BCD bytes to uint32_t
+    uint32_t DecodeBCD(uint8_t* ps, int how_many) const;
+    bool CRCInvalid(uint8_t* buf, int sz) const;
 
     std::unordered_map<uint32_t, TEnergyValues> EnergyCache;
     std::unordered_map<uint32_t, TParamValues> ParamCache;
-
-    static const std::array<char, 10> DIGITS;
 };
 
 typedef std::shared_ptr<TMercury20002Device> PMercury20002Device;
--- wb-mqtt-serial/serial_port.cpp
+++ wb-mqtt-serial/serial_port.cpp
@@ -33,7 +33,11 @@ TLibModbusContext::TLibModbusContext(const TSerialPortSettings& settings)
         struct timeval tv;
         tv.tv_sec = response_timeout.count() / 1000;
         tv.tv_usec = (response_timeout.count() % 1000) * 1000;
+#ifdef __APPLE__
         modbus_set_response_timeout(Inner, tv.tv_sec, tv.tv_usec);
+#else
+        modbus_set_response_timeout(Inner, &tv);
+#endif
     }
 }
 
